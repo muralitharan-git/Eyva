98a0cc8203ffce1a2c96cd84cf5c87b1b76490bd
Muralitharan <muralitharan.govindaraj00@gmail.com>
2021-12-29 13:50:55 +0530

    Bluetooth storyboard added

--- Eyva/Dashboard/ViewControllers/HomeViewController.swift
+++ Eyva/Dashboard/ViewControllers/HomeViewController.swift
@@ -19,12 +19,12 @@ class HomeViewController: UIViewController {
     }
 }
 
-extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-    }
-}
+//extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
+////    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+////        return 2
+////    }
+////
+////    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+////
+////    }
+//}
