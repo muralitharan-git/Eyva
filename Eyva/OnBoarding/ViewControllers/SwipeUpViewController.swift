36e33edaeb7bc3daaab4345a7955069cb412aaf7
Muralitharan <muralitharan.govindaraj@emids.com>
2022-01-01 21:54:48 +0530

    Font update

--- Eyva/OnBoarding/ViewControllers/SwipeUpViewController.swift
+++ Eyva/OnBoarding/ViewControllers/SwipeUpViewController.swift
@@ -8,9 +8,18 @@
 import UIKit
 
 class SwipeUpViewController: UIViewController {
-
+   
+    @IBOutlet weak var swipeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        swipeButton.setTitle("Enter to Anthea Realm", for: .normal)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        swipeButton.titleLabel?.font = UIFont(name: "Mulish-Regular", size: 18)
+        swipeButton.setTitleColor(rgb(244, 238, 254, 1.0), for: .normal)
     }
     
 
