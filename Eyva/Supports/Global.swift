83faf1b55df5fe78ee7fa21de79c4d97227d6d1e
Muralitharan <muralitharan.govindaraj@emids.com>
2022-01-02 14:57:40 +0530

    icons updated

--- Eyva/Supports/Global.swift
+++ Eyva/Supports/Global.swift
@@ -18,13 +18,13 @@ func rgb(_ r: Int, _ g: Int, _ b: Int, _ a: CGFloat) -> UIColor {
 
 // Gradient
 let pinkGradientBG = [
-    UIColor(red: 0.718, green: 0.478, blue: 0.733, alpha: 1).cgColor,
-    UIColor(red: 0.937, green: 0.678, blue: 0.788, alpha: 1).cgColor
+    UIColor(red: 0.718, green: 0.478, blue: 0.733, alpha: 0.7).cgColor,
+    UIColor(red: 0.937, green: 0.678, blue: 0.788, alpha: 0.7).cgColor
 ]
 
 let blueGradientBG = [
-    UIColor(red: 0.337, green: 0.388, blue: 0.549, alpha: 1).cgColor,
-    UIColor(red: 0.443, green: 0.71, blue: 0.827, alpha: 1).cgColor
+    UIColor(red: 0.337, green: 0.388, blue: 0.549, alpha: 0.7).cgColor,
+    UIColor(red: 0.443, green: 0.71, blue: 0.827, alpha: 0.7).cgColor
 ]
 
 let brownGradientBG = [
--- Eyva/Supports/GradientBackgroundView.swift
+++ Eyva/Supports/GradientBackgroundView.swift
@@ -12,10 +12,12 @@ class GradientBackgroundView: UIView {
     internal func setColors(_ colors: [CGColor]) {
         let layer0 = CAGradientLayer()
         layer0.colors = colors
-        layer0.locations = [0, 1]
+        layer0.locations = [0.28, 1]
         layer0.startPoint = CGPoint(x: 0.25, y: 0.5)
         layer0.endPoint = CGPoint(x: 0.75, y: 0.5)
-        layer0.transform = CATransform3DMakeAffineTransform(CGAffineTransform(a: -0.67, b: 1, c: -1, d: -0.67, tx: 1.35, ty: 0.34))
+        layer0.transform = CATransform3DMakeAffineTransform(CGAffineTransform(a: -1.21, b: 0, c: 0, d: -5.54, tx: 1.21, ty: 2.77))
+        
+        
         layer0.bounds = self.bounds.insetBy(dx: -0.5 * self.bounds.size.width, dy: -0.5 * self.bounds.size.height)
         layer0.position = self.center
         self.layer.insertSublayer(layer0, at: 0)
